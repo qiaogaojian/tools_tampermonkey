cfac19cd3c55b4195b0e6226160b55c10f14c5e9
qiaogaojian <qiaogaojian@vip.qq.com>
2020-05-09 11:36:52 +0800

    移除百度热搜

--- Baidu+Google.js
+++ Baidu+Google.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name              百度+Google
+// @name              纯净百度+Google
 // @name:en           Baidu+Google
 // @namespace         http://mofiter.com/
 // @version           0.1
-// @description       在百度首页和搜索结果页面的百度一下按钮后面添加 Google 按钮，方便直接进行 Google 搜索
+// @description       去除百度热搜和上下栏,在百度首页和搜索结果页面的百度一下按钮后面添加 Google 按钮，方便直接进行 Google 搜索
 // @description:en    add a google search button behind the baidu search button on the baidu's homepage and search result page,making it convenient to search in google
 // @author            mofiter
 // @create            2020-04-20
@@ -15,24 +15,53 @@
 
 (function() {
     'use strict';
+
+
+    // 隐藏百度热搜和上下栏
+    if(document.getElementById('qrcode')!=null)
+    {
+        document.getElementById('qrcode').style.visibility='hidden'
+    }
+    if(document.getElementById('bottom_layer')!=null)
+    {
+        document.getElementById('bottom_layer').style.visibility='hidden'
+    }
+    if(document.getElementById('u1')!=null)
+    {
+        document.getElementById('u1').style.visibility='hidden'
+    }
+    if(document.getElementById('s-hotsearch-wrapper')!=null)
+    {
+        document.getElementById('s-hotsearch-wrapper').style.visibility='hidden'
+    }
+    if(document.getElementById('hotsearch-content-wrapper')!=null)
+    {
+        document.getElementById('hotsearch-content-wrapper').style.visibility='hidden'
+    }
+    if(document.getElementById('s-top-left')!=null)
+    {
+        document.getElementById('s-top-left').style.visibility='hidden'
+    }
+
+
+    // 添加Google搜索
     document.getElementById("form").style.width = "1000px";
     document.getElementsByClassName("s_btn_wr")[0].style.width = "80px";
+
     var baiduBtn = document.getElementById("su"); // 百度搜索按钮
-    baiduBtn.style = "width:80px; border-radius:0;";
     var googleBtn = document.createElement('span'); // Google 搜索按钮
+    baiduBtn.style = "width:80px; border-radius:0;";
     googleBtn.className = baiduBtn.parentNode.className; // 将 Google 搜索按钮和百度搜索按钮的 class 名称设置为相同
     googleBtn.style = "width:80px;margin:0px 0px 0px 2px;";
-    var h = baiduBtn.offsetHeight;
 
+    var h = baiduBtn.offsetHeight;
     var getStyle = function(element, property) {
     return window.getComputedStyle ? window.getComputedStyle(element, null).getPropertyValue(property) : element.style[property.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); })];
     };
     var color = getStyle(baiduBtn, "background-color");
     var baiduUrl = window.location.href;
-    // window.alert(baiduUrl);
     if (baiduUrl.indexOf("/s?")!= -1){
         googleBtn.innerHTML = "<input type='button' id='google' value='Google' class='btn' style='width:80px; height:"+h+"px; color:#fff; background:"+ color +";border:0;border-radius:0 10px 10px 0;'>";
-        // window.alert("finish");
     }else{
         googleBtn.innerHTML = "<input type='button' id='google' value='Google' class='btn' style='width:80px; height:"+h+"px;color:#fff; background:"+ color +";border-radius:0 10px 10px 0;'>";
     }
@@ -44,6 +73,7 @@
             return googleSearch(keyword);
         }
     })
+
     var form = document.getElementsByClassName("fm")[0];
     form.appendChild(googleBtn);
 
@@ -52,14 +82,4 @@
         // window.location.href = link; //当前窗口打开链接
         window.open(link); //新窗口打开链接
     }
-    document.getElementById('qrcode').style.visibility='hidden'
-    document.getElementById('bottom_layer').style.visibility='hidden'
-    document.getElementById('u1').style.visibility='hidden'
-
-    //baiduBtn.addEventListener('click',onClickBaidu);
-    //function onClickBaidu()
-    //{
-    //    googleBtn.style.visibility = hidden;
-    //    googleBtn.innerHTML = "<input type='button' id='google' value='Google' class='btn' style='width:80px; height:60px;'>";
-    //}
 })();
