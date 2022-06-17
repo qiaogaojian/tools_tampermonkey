45b8b3c8bc20b60c30b780d2d840de8757b9cf30
Gaojian <qiaogaojian@vip.qq.com>
2022-06-17 09:34:31 +0800

    Update Baidu+Google.js

--- Baidu+Google.js
+++ Baidu+Google.js
@@ -2,12 +2,10 @@
 // @name              纯净百度+Google
 // @name:en           Baidu+Google
 // @namespace         http://mofiter.com/
-// @version           0.1
+// @version           1.0
 // @description       去除百度热搜和上下栏,在百度首页和搜索结果页面的百度一下按钮后面添加 Google 按钮，方便直接进行 Google 搜索
 // @description:en    add a google search button behind the baidu search button on the baidu's homepage and search result page,making it convenient to search in google
 // @author            mofiter
-// @create            2020-04-20
-// @lastmodified      2020-04-20
 // @match             http*://www.baidu.com/
 // @match             http*://www.baidu.com/s?*
 // @match             *://*.sogou.com/*
@@ -16,16 +14,6 @@
 
 (function() {
     'use strict';
-
-    // 隐藏百度热搜和上下栏
-    hideById("qrcode")
-    hideById("bottom_layer")
-    hideById("u1")
-    hideById("s-hotsearch-wrapper")
-    hideById("hotsearch-content-wrapper")
-    hideById("s-top-left")
-    hideById("hotword")
-    hideById("content_right")
 
     if(document.getElementById("s_main")!=null){
            document.getElementById("s_main").innerHTML=''
@@ -68,9 +56,9 @@
     var color = getStyle(baiduBtn, "background-color");
     var baiduUrl = window.location.href;
     if (baiduUrl.indexOf("/s?")!= -1){
-        showStyle1()
+        showStyle1();
     }else{
-        showStyle2()
+        showStyle2();
     }
     var input = document.getElementById("kw"); // 百度输入框
     input.oninput = function(){
@@ -82,10 +70,10 @@
         setTimeout(function(){
             var h = baiduBtn.offsetHeight;
 
-            btn1.style.height=h + "px"
-            btn2.style.height=h + "px"
-            btn3.style.height=h + "px"
-            btn4.style.height=h + "px"
+            btn1.style.height=h + "px";
+            btn2.style.height=h + "px";
+            btn3.style.height=h + "px";
+            btn4.style.height=h + "px";
         }, 10);
 	}
 
@@ -148,17 +136,17 @@
 
     function weixinSearch(keyword){
         var link = "https://weixin.sogou.com/weixin?type=2&query=" + encodeURIComponent(keyword);
-        window.open(link)
+        window.open(link);
     }
 
     function bilibiliSearch(keyword){
         var link = "https://search.bilibili.com/all?keyword=" + encodeURIComponent(keyword);
-        window.open(link)
+        window.open(link);
     }
 
     function zhihuSearch(keyword){
         var link = "https://www.zhihu.com/search?type=content&q=" + encodeURIComponent(keyword);
-        window.open(link)
+        window.open(link);
     }
     baiduBtn.addEventListener('click', function () {
     var input = document.getElementById("kw"); // 百度输入框
@@ -173,4 +161,21 @@
         // window.location.href = link; //当前窗口打开链接
         window.open(link,'_self'); //新窗口打开链接
     }
+
+    function hideUnUsed(){
+        // 隐藏百度热搜和上下栏
+        hideById("qrcode");
+        hideById("bottom_layer");
+        hideById("u1");
+        hideById("s-hotsearch-wrapper");
+        hideById("hotsearch-content-wrapper");
+        hideById("s-top-left");
+        hideById("hotword");
+        hideById("content_right");
+    }
+
+    hideUnUsed();
+    setTimeout(function(){ // 确保一定能隐藏需要隐藏的东西
+        hideUnUsed();
+    }, 10);
 })();
